ca79a14883f3a5e0f6370eaf0c01045eee4b8387
Modesto <mofesdo.dev@gmail.com>
2024-10-02 06:00:26 -0700

    modal button functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,14 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+
+profileEditButton.addEventListener("click", function(){
+  editProfileModal.classList.add("modal__opened");
+});
+
+editProfileModal.addEventListener("click", function(){
+  editProfileModal.classList.remove("modal__opened");
+});
